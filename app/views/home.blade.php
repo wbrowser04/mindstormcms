01e33be3386768bddaf9283d6123c4c3b49cb20d
unknown <wbrowser04>
2014-12-11 15:50:39 +0800

    added bootstrap

---
+++ app/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('frontend.master')
+
+@section('header')
+<h1>Header</h1>
+@stop
+@section('content')
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="#" role="button">View navbar docs &raquo;</a>
+        </p>
+      </div>
+@stop
+@section('footer')
+Copyright &copy; 2014 Mindstorm Creations. All rights reserved.
+@stop
